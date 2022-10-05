fe3cd1f39bf8fa8dc7c2c1ceaf5385aed18f0dd9
Adel <a.adel.a.fattah@gmail.com>
2022-10-06 00:23:07 +0200

    Update FakeDataSource.kt

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,9 +4,12 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
+
+// A class to replace the actual data source to act as a test double
 class FakeDataSource(private val reminders: MutableList<ReminderDTO>? = mutableListOf()) :
     ReminderDataSource {
-
+    // Over riding the get reminders method to make it return success if there is any reminders
+    // and returns error if null
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         reminders?.let { return Result.Success(ArrayList(it)) }
         return Result.Error(
@@ -14,10 +17,12 @@ class FakeDataSource(private val reminders: MutableList<ReminderDTO>? = mutableL
         )
     }
 
+    // Overriding the method in ReminderDataSource to save the reminder in the mutable list Reminders
     override suspend fun saveReminder(reminder: ReminderDTO) {
         reminders?.add(reminder)
     }
 
+    // Overriding to get the reminder using the filter method
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         reminders?.let {
             val res = reminders.filter { it.id == id }
@@ -34,6 +39,7 @@ class FakeDataSource(private val reminders: MutableList<ReminderDTO>? = mutableL
         )
     }
 
+    // Clearing the reminders mutable list
     override suspend fun deleteAllReminders() {
         reminders?.clear()
     }
